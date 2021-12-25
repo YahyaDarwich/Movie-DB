38b903d79f48c39994329023164708cd46367921
Yahya <ydarwich18@gmail.com>
2021-12-25 16:56:16 +0200

    step 3

--- index.js
+++ index.js
@@ -2,8 +2,31 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+var today = new Date();
+var time =
+  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
 app.get("/", (req, res) => {
   res.send("ok");
+  next();
+});
+
+app.get("/test", (req, res) => {
+  res.status(200);
+  res.send("ok");
+
+  // OR using this method
+  // res.status(200).send("ok");
+  next();
+});
+
+app.get("/time", (req, res) => {
+  res.status(200);
+  res.send(time);
+
+  // OR using this method
+  // res.status(200).send(time);
+  next();
 });
 
 app.listen(port, () => {
