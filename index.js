e4a3ea70aff74ba8f9474a2f1a03950b6eaa81ac
Yahya <ydarwich18@gmail.com>
2021-12-26 18:32:26 +0200

    step 4

--- index.js
+++ index.js
@@ -29,6 +29,34 @@ app.get("/time", (req, res) => {
   next();
 });
 
+app.get("/hello/:id", function (req, res, next) {
+  console.log("id: " + req.params.id);
+  if (+req.params.id) {
+    console.log("Yes it's ID: " + +req.params.id);
+    res.status(200).send("Hello, " + req.params.id);
+    next();
+  } else {
+    console.log("No it's not ID: " + +req.params.id);
+    res.status(500).send("Error: id must be integer " + req.params.id);
+    next();
+  }
+});
+
+app.get("/search", function (req, res, next) {
+  var data = { data: req.param("s") };
+  console.log(req.param("s"));
+  let str = req.param("s");
+  if (str == null || str == undefined || str.length == 0) {
+    res.status(500).send("you have to provide a search");
+    throw new Error("true");
+    next();
+  } else {
+    res.status(200).send("ok " + req.param("s"));
+    res.json(data);
+    next();
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
